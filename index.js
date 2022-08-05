27c812c533905dae513d9fb8994a50e2ece17ff3
AlyciaSilv <106041841+AlyciaSilv@users.noreply.github.com>
2022-08-05 10:19:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ async function main() {
   app.use(express.json());
 
   app.get("/", function (req, res) {
-    res.send("<h1>Olá Papai! coloque '/oi' no final da url para ver a outra mensagem! <button href='https://ocean-alycia-heroku-22-07.herokuapp.com/oi'>Click Here</button>");
+    res.send("<h1>Olá Papai! coloque '/oi' no final da url para ver a outra mensagem! <button href='/oi'>Click Here</button>");
   });
 
   // /oi -> "Olá, mundo"
